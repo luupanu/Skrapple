621b17aa2323c94c2a7d2cbeb010e942ac0878cf
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2017-02-03 23:02:01 +0200

    added SquareTest

---
+++ Skrapple/src/test/java/fi/luupanu/skrapple/core/SquareTest.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.luupanu.skrapple.core;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author panu
+ */
+public class SquareTest {
+
+    private Square normal;
+    private Letter let;
+
+    @Before
+    public void setUp() {
+        normal = new Square(SquareType.SQUARE_NORMAL);
+        let = new Letter(LetterType.LETTER_E);
+    }
+
+    @Test
+    public void hasLetterReturnsFalseWhenSquareIsCreated() {
+        assertEquals(false, normal.hasLetter());
+    }
+
+    @Test
+    public void hasLetterReturnsTrueWhenSquareHasALetter() {
+        normal.placeLetter(let);
+        assertEquals(true, normal.hasLetter());
+    }
+
+    @Test
+    public void getLetterReturnsNullWhenSquareIsEmpty() {
+        assertEquals(null, normal.getLetter());
+    }
+
+    @Test
+    public void getLetterReturnsLetterWhenSquareHasALetter() {
+        normal.placeLetter(let);
+        assertEquals(let, normal.getLetter());
+    }
+    
+    @Test
+    public void placeLetterWhenSquareHasALetterFails() {
+        normal.placeLetter(let);
+        normal.placeLetter(new Letter(LetterType.LETTER_H));
+        assertEquals(let, normal.getLetter());
+    }
+}
