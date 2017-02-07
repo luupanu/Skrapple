89b3b18de62aa5de2e1e64454026e6761dcaba4e
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2017-02-07 23:55:13 +0200

    WordChecker done

--- Skrapple/src/main/java/fi/luupanu/skrapple/logic/WordChecker.java
+++ Skrapple/src/main/java/fi/luupanu/skrapple/logic/WordChecker.java
@@ -5,6 +5,9 @@
  */
 package fi.luupanu.skrapple.logic;
 
+import fi.luupanu.skrapple.domain.Dictionary;
+import java.util.List;
+
 /**
  *
  * @author panu
@@ -13,4 +16,19 @@ public class WordChecker {
     /*  TO-DO: a working class WordChecker. WordChecker will check if the
         to-be-added words actually form something meaningful using a dictionary.
     */
+    
+    private Dictionary d;
+    
+    public WordChecker(Dictionary d) {
+        this.d = d;
+    }
+    
+    public boolean allWordsExistInDictionary(List<String> words) {
+        for (String w : words) {
+            if (!d.containsWord(w)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
