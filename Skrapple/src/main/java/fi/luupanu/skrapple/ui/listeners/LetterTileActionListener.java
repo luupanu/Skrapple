530e3a91d5455ec30cf70e6ff839f2a88618e606
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2017-02-23 09:04:59 +0200

    added wild letter support

--- Skrapple/src/main/java/fi/luupanu/skrapple/ui/listeners/LetterTileActionListener.java
+++ Skrapple/src/main/java/fi/luupanu/skrapple/ui/listeners/LetterTileActionListener.java
@@ -3,18 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fi.luupanu.skrapple.ui;
+package fi.luupanu.skrapple.ui.listeners;
 
 import fi.luupanu.skrapple.constants.LetterType;
 import fi.luupanu.skrapple.domain.Coord;
 import fi.luupanu.skrapple.domain.Letter;
+import fi.luupanu.skrapple.domain.WildLetter;
 import fi.luupanu.skrapple.logic.SkrappleGame;
+import fi.luupanu.skrapple.ui.components.LetterTile;
+import fi.luupanu.skrapple.ui.components.WildLetterTypeDialog;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
@@ -22,39 +24,31 @@ import javax.swing.border.LineBorder;
  *
  * @author panu
  */
-public class CustomActionListener implements ActionListener {
+public class LetterTileActionListener implements ActionListener {
 
     private Border greenBorder = new LineBorder(Color.GREEN);
     private Border whiteBorder = new LineBorder(Color.WHITE);
 
     private JFrame frame;
-    private JButton move;
-    private JButton skip;
-    private JButton exchange;
-    private JButton resign;
-    private JButtonLetter[][] boardSquares;
-    private JButtonLetter[] rackLetters;
-    private JButtonLetter selected;
+    private LetterTile[][] boardSquares;
+    private LetterTile[] rackLetters;
+    private LetterTile selected;
     private SkrappleGame s;
 
-    public CustomActionListener(SkrappleGame s, JFrame frame, JButtonLetter[][] boardSquares,
-            JButtonLetter[] rackLetters, JButton move, JButton skip, JButton exchange,
+    public LetterTileActionListener(SkrappleGame s, JFrame frame, LetterTile[][] boardSquares,
+            LetterTile[] rackLetters, JButton move, JButton skip, JButton exchange,
             JButton resign) {
         this.s = s;
         this.frame = frame;
         this.boardSquares = boardSquares;
         this.rackLetters = rackLetters;
-        this.move = move;
-        this.skip = skip;
-        this.exchange = exchange;
-        this.resign = resign;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() instanceof JButtonLetter) {
+        if (e.getSource() instanceof LetterTile) {
             // the button the user clicked
-            JButtonLetter b = (JButtonLetter) e.getSource();
+            LetterTile b = (LetterTile) e.getSource();
 
             /* IF THE BUTTON CLICKED IS A RACK BUTTON */
             if (!b.getIsBoardLetter()) {
@@ -82,8 +76,12 @@ public class CustomActionListener implements ActionListener {
             // if the selected button already has a letter, put it back to rack
             if (b.getLetter() != null) {
                 Letter let = s.getGame().getLetterQueue().takeLetterFromQueue(b.getLetter());
+                // reset a wild letter before placing it back
+                if (let.getType() == LetterType.LETTER_WILD) {
+                    resetWildLetterType(let);
+                }
                 s.getGame().getCurrentPlayer().getPlayerRack().addLetter(let);
-                for (JButtonLetter jbl : rackLetters) {
+                for (LetterTile jbl : rackLetters) {
                     if (jbl.getLetter() == null) {
                         jbl.setLetter(let);
                         b.paintBoardIcon(s.getGame().getBoard().getLayout());
@@ -95,6 +93,12 @@ public class CustomActionListener implements ActionListener {
             // if a rack letter has been selected, try to place it in the board
             if (selected != null) {
                 Letter let = selected.getLetter();
+                if (let.getType() == LetterType.LETTER_WILD) {
+                    // if the user does not provide a wild letter type, do nothing
+                    if (!askWildLetterType(let)) {
+                        return;
+                    }
+                }
                 if (s.getGame().getLetterQueue().addLetterToQueue(let, b.getCoord(), s.getGame().getBoard())) {
                     s.getGame().getCurrentPlayer().getPlayerRack().takeLetter(let);
                     b.setLetter(let);
@@ -104,17 +108,23 @@ public class CustomActionListener implements ActionListener {
                     paintValidMoves();
                 }
             }
-        } else if (e.getSource() == move) {
-            System.out.println("wut1");
-        } else if (e.getSource() == skip) {
-            System.out.println("wut2");
-            askConfirmation("Skip turn");
-        } else if (e.getSource() == exchange) {
-            System.out.println("wut3");
-        } else if (e.getSource() == resign) {
-            System.out.println("wut4");
-            askConfirmation("Resign");
         }
+    }
+
+    private boolean askWildLetterType(Letter let) {
+        WildLetter wLet = (WildLetter) let;
+        WildLetterTypeDialog wltd = new WildLetterTypeDialog();
+        LetterType type = wltd.askWildLetterType(frame);
+        if (type == null) {
+            return false;
+        }
+        wLet.setWildLetterType(type);
+        return true;
+    }
+
+    private void resetWildLetterType(Letter let) {
+        WildLetter wLet = (WildLetter) let;
+        wLet.setWildLetterType(null);
     }
 
     private void paintValidMoves() {
@@ -130,19 +140,6 @@ public class CustomActionListener implements ActionListener {
                     boardSquares[x][y].setBorder(whiteBorder);
                 }
             }
-        }
-    }
-
-    private void askConfirmation(String msg) {
-        String defaultMessage = "Are you sure you want to " + msg.toLowerCase() + "?";
-        int response = JOptionPane.showConfirmDialog(frame, defaultMessage, msg + "?",
-                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-        if (response == JOptionPane.NO_OPTION) {
-            System.out.println("No!");
-        } else if (response == JOptionPane.YES_OPTION) {
-            System.out.println("Yes!");
-        } else if (response == JOptionPane.CLOSED_OPTION) {
-            System.out.println("Closed!");
         }
     }
 }
