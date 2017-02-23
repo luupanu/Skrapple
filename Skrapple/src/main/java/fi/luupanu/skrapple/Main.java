9164fc509bc157690bef0159ab44be02a3e4a82d
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2017-02-23 08:47:19 +0200

    renamed GUI

--- Skrapple/src/main/java/fi/luupanu/skrapple/Main.java
+++ Skrapple/src/main/java/fi/luupanu/skrapple/Main.java
@@ -15,7 +15,7 @@ import javax.swing.SwingUtilities;
 public class Main {
 
     public static void main(String[] args) {
-        
+
         SwingUtilities.invokeLater(new SkrappleGUI());
     }
 }
