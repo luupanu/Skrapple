43cfc9bff2bfbfca14c4bd969889a855101665bf
Panu Luukkonen <panu.j.luukkonen@helsinki.fi>
2017-02-26 16:25:35 +0200

    test for word list empty added

--- Skrapple/src/test/java/fi/luupanu/skrapple/domain/DictionaryTest.java
+++ Skrapple/src/test/java/fi/luupanu/skrapple/domain/DictionaryTest.java
@@ -5,8 +5,11 @@ package fi.luupanu.skrapple.domain;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -19,24 +22,24 @@ import static org.junit.Assert.*;
  * @author panu
  */
 public class DictionaryTest {
-    
+
     private Dictionary d;
     private List<String> words;
 
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
         d = new Dictionary("kotus-wordlist-fi");
         words = d.getWordList();
     }
-    
+
     @Test
     public void wordListContains84420Words() {
         assertEquals(84420, words.size());
     }
-    
-//    @Test
-//    public void getWordListReturnsNullWhenFileNotFound() {
-//        Dictionary dic = new Dictionary("drumpf");
-//        assertEquals(null, dic.getWordList());
-//    }
+
+    @Test
+    public void getWordListReturnsEmptyListWhenFileNotFound() throws IOException {
+        Dictionary dic = new Dictionary("drumpf");
+        assertEquals(0, dic.getWordList().size());
+    }
 }
